ea9edabb1abcade0c9c842826434180d8c4bee92
Penzin Konstantin <p.kostik@bk.ru>
2022-10-30 18:47:41 +0400

    fix

--- src/Indexing.php
+++ src/Indexing.php
@@ -81,7 +81,7 @@ class Indexing
      */
     private function result(object $resp)
     {
-        return (string) $resp->getBody();
+        return json_decode((string) $resp->getBody(), true);
     }
 
     /**
